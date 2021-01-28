32042880d993a7fec5c8687ffc2bd73f665b8ef0
Stevche Radevski <sradevski@live.com>
2021-01-28 13:43:02 +0100

    Add ORAS to Dockerfile for runner
    Change-type: minor
    Signed-off-by: Stevche Radevski <stevche@balena.io>

--- transformer-runner/src/mock-jellyfish-client-sdk.ts
+++ transformer-runner/src/mock-jellyfish-client-sdk.ts
@@ -1,49 +1,56 @@
-import { EventEmitter } from 'events';
-import * as fs from 'fs';
+import { EventEmitter } from "events";
+import * as fs from "fs";
+import * as path from "path";
 
 export function getSdk(_apiConfig: any) {
-    return sdk;
+  return sdk;
 }
 
 const sdk = {
-    auth: {
-        whoami: async () => {
-            return {
-                id: '1111-1111-1111-1111',
-                slug: 'worker-1111-1111-1111-1111',
-            }
-        },
+  auth: {
+    whoami: async () => {
+      return {
+        id: "1111-1111-1111-1111",
+        slug: process.env.WORKER_SLUG || "worker-1111-1111-1111-1111",
+      };
     },
-    card: {
-        create: (_data: any) => {
-            return {
-                id: '1234-1234-1234-1234',
-                slug: 'card-1234-1234-1234-1234'
-            }
-        },
-        get: (_id: string) => {
-            return {
-                id: '1234-1234-1234-1234',
-                slug: 'card-1234-1234-1234-1234'
-            }
-        },
-        update: (_id: string, _type: string, _data: any) => {},
+  },
+  card: {
+    create: (_data: any) => {
+      return {
+        id: "1234-1234-1234-1234",
+        slug: "card-1234-1234-1234-1234",
+      };
     },
-    setAuthToken: async (_authToken: string) => {},
-    stream: async (_schema: any) => {
-        const interval = 5000;
-        
-        const task = JSON.parse(
-            await fs.promises.readFile('./task.json', 'utf8')
-        );
-        
-        const taskEmitter = new EventEmitter();
-        setInterval(() => {
-            taskEmitter.emit('update', task);
-        }, interval)
-        
-        return taskEmitter; 
+    get: (_id: string) => {
+      return {
+        id: "1234-1234-1234-1234",
+        slug: "card-1234-1234-1234-1234",
+      };
     },
-}
+    update: (_id: string, _type: string, _data: any) => {},
+  },
+  setAuthToken: async (_authToken: string) => {},
+  stream: async (_schema: any) => {
+    const interval = 15000;
 
+    let task = "";
+    try {
+      task = JSON.parse(
+        await fs.promises.readFile(
+          path.join(process.cwd(), "mocks", "task.json"),
+          "utf8"
+        )
+      );
+    } catch (e) {
+      console.log(e);
+    }
 
+    const taskEmitter = new EventEmitter();
+    setInterval(() => {
+      taskEmitter.emit("update", task);
+    }, interval);
+
+    return taskEmitter;
+  },
+};

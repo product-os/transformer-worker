43ac818ceee1486997394f497228d7aaaa1ea51d
Stevche Radevski <sradevski@live.com>
2021-02-01 13:56:02 +0100

    Use dind for the transformer runner
    Change-type: patch
    Signed-off-by: Stevche Radevski <stevche@balena.io>

--- transformer-runner/entrypoint.sh
+++ transformer-runner/entrypoint.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/sh
 
 device_uuid=$(curl -s "$BALENA_SUPERVISOR_ADDRESS/v1/device/host-config?apikey=$BALENA_SUPERVISOR_API_KEY" | jq -r .network.hostname)
 
@@ -8,4 +8,5 @@ fi
 
 export WORKER_JF_TOKEN=$(cat /shared/.token)
 export WORKER_SLUG="transformer-worker-${device_uuid}"
-npm start
+
+dockerd &> dockerd-output.log & sleep 1 && npm start

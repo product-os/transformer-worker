64dabe837ce61cbc0ff6437a3d1a6dc9f54d69de
Martin Rauscher <martin@balena.io>
2021-02-04 11:02:58 +0000

    simplify getting of device UUID
    Change-type: patch
    Signed-off-by: Martin Rauscher <martin@balena.io>

--- transformer-runner/entrypoint.sh
+++ transformer-runner/entrypoint.sh
@@ -1,12 +1,10 @@
 #!/bin/sh
 
-device_uuid=$(curl -s "$BALENA_SUPERVISOR_ADDRESS/v1/device/host-config?apikey=$BALENA_SUPERVISOR_API_KEY" | jq -r .network.hostname)
-
 if [ ! -e /shared/.token ]; then
     uuidgen > /shared/.token
 fi
 
 export WORKER_JF_TOKEN=$(cat /shared/.token)
-export WORKER_SLUG="transformer-worker-${device_uuid}"
+export WORKER_SLUG="transformer-worker-${BALENA_DEVICE_UUID}"
 
 dockerd &> dockerd-output.log & sleep 1 && npm start

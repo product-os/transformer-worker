fc047b20785f6af447dbc673d6cceea671410096
Martin Rauscher <martin@balena.io>
2021-02-18 14:54:53 +0100

    dev-artifact-provider: add artifactReady handling
    Signed-off-by: Martin Rauscher <martin@balena.io>

--- dev-artifact-provider/entrypoint.sh
+++ dev-artifact-provider/entrypoint.sh
@@ -68,4 +68,29 @@ popd
 
 echo "finished importing artifacts"
 
+echo "linking cards"
+
+loop_id=$(curl 'http://api.ly.fish.local/api/v2/slug/loop-product-os' \
+  -H "Authorization: Bearer $jf_auth_token" | jq -j .id)
+
+curl 'http://api.ly.fish.local/api/v2/action' \
+  -H "Authorization: Bearer $jf_auth_token" \
+  -H 'Content-Type: application/json;charset=UTF-8' \
+  --data-raw '{"card":"link","type":"type","action":"action-create-card@1.0.0","arguments":{"reason":null,"properties":{"slug":"link-poructos-owns-identity-transformer","tags":[],"version":"1.0.0","links":{},"requires":[],"capabilities":[],"active":true,"name":"owns","data":{"inverseName":"is owned by","from":{"id":"'$loop_id'","type":"loop@1.0.0"},"to":{"id":"fb3b504c-5f3c-462f-bff8-0c8cc26e2443","type":"transformer@1.0.0"}}}}}' \
+  || echo "link already exists"
+
+echo "marking artifacts as ready"
+
+for c in ls ./*/*/card.json ; do
+
+	id=$(cat $c | jq -j .arguments.properties.id)
+
+	curl 'http://api.ly.fish.local/api/v2/action' \
+	-H 'Accept: application/json' \
+	-H "Authorization: Bearer $jf_auth_token" \
+	-H 'Content-Type: application/json;charset=UTF-8' \
+	--data-raw '{"card":"'$id'","type":"faq@1.0.0","action":"action-update-card@1.0.0","arguments":{"reason":null,"patch":[{"op":"replace","path":"/data/$transformer/artifactReady","value":true}]}}'
+
+done
+
 tail -f /dev/null

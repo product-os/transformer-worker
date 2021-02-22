3ebc64192753aa39cc902d5fc7dacf2be2b1371c
Martin Rauscher <martin@balena.io>
2021-02-22 13:51:29 +0100

    fleet: automatically setup enrollment secret
    Change-type: patch
    Signed-off-by: Martin Rauscher <martin@balena.io>

--- fleet-launcher/entrypoint.sh
+++ fleet-launcher/entrypoint.sh
@@ -3,4 +3,16 @@
 device_uuid=$(curl -s "$BALENA_SUPERVISOR_ADDRESS/v1/device/host-config?apikey=$BALENA_SUPERVISOR_API_KEY" | jq -r .network.hostname)
 export WORKER_JF_TOKEN="$(cat /shared/.token)"
 
-launcher --hostname=${FLEET_HOSTNAME} --root_directory=$(mktemp -d) --enroll_secret=${FLEET_ENROLL_SECRET} --osquery_flag docker_socket=/var/run/balena-engine.sock --osquery_flag host-identifier=specified --osquery_flag specified-identifier="$device_uuid" ${FLEET_EXTRA_ARGS}
+while [ "x" == "x$FLEET_ENROLL_SECRET" ]; do
+	echo "waiting for enroll secret..."
+	sleep 1
+	export FLEET_ENROLL_SECRET=$(cat /shared/.enroll_secret)
+done
+
+launcher --hostname=${FLEET_HOSTNAME} \
+	--root_directory=$(mktemp -d) \
+	--enroll_secret=${FLEET_ENROLL_SECRET} \
+	--osquery_flag docker_socket=/var/run/balena-engine.sock \
+	--osquery_flag host-identifier=specified \
+	--osquery_flag specified-identifier="$device_uuid" \
+	${FLEET_EXTRA_ARGS}

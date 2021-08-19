d71e294afb7ec5f29709491d3ec73a1af766acb4
Dan Goodman <danthegoodman@icloud.com>
2021-08-19 12:31:22 -0400

    remove secrets
    Change-type: patch

--- transformer-runner/src/runner.ts
+++ transformer-runner/src/runner.ts
@@ -299,35 +299,3 @@ async function runTask(task: TaskContract) {
 
 	console.log(`[WORKER] Task ${task.slug} completed successfully`);
 }
-
-/**
- * This function takes an object tree with all string values expected to be
- * base64 encoded secrets and returns the same tree with the values decrypted
- * but again base64 encoded.
- * (The latter allows passing binary secrets as well)
- *
- * @param encryptedSecrets object that only contains string values or other encryptedSecrets objects
- */
-export function decryptSecrets(secretsKey: NodeRSA | undefined, sec: any): any {
-	if (!sec) {
-		return undefined;
-	}
-	if (!secretsKey) {
-		console.log(
-			`WARN: no secrets key provided! Will pass along secrets without decryption. Should not happen in production`,
-		);
-		return sec;
-	}
-	let result: any = {};
-	for (const key of Object.keys(sec)) {
-		const val = sec[key];
-		if (typeof val === 'string') {
-			result[key] = secretsKey.decrypt(val, 'base64');
-		} else if (typeof val === 'object') {
-			result[key] = exports.decryptSecrets(secretsKey, val);
-		} else {
-			console.log(`WARN: unknown type in secrets for key ${key}`);
-		}
-	}
-	return result;
-}

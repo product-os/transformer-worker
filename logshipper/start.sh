6577f555a9a502cf7ea806d45c3c113a05c28f66
Carlo Miguel F. Cruz <carloc@balena.io>
2022-04-14 16:35:17 +0000

    logshipper: downgrade Vector to v0.18.1.
    Any newer versions are having reliability issues such as
    logs not being shipped to the backend.
    
    Change-type: patch
    Signed-off-by: Carlo Miguel F. Cruz <carloc@balena.io>

--- logshipper/start.sh
+++ logshipper/start.sh
@@ -1,32 +1,43 @@
 #!/usr/bin/env bash
+# shellcheck disable=SC2034,SC2086
 
-set -e
+set -ae
 
 cd /etc/vector/
 
-export BALENA_FLEET_NAME=${BALENA_APP_NAME}
-export CERTIFICATES_DIR="/etc/vector/certificates"
-CONFIG_FILES="vector.yaml"
+BALENA_FLEET_NAME=${BALENA_APP_NAME}
+CERTIFICATES_DIR=/etc/vector/certificates
+CONFIG_FILES=vector.yaml
+VECTOR_TLS_ENABLED=false
 
-if [ ! -z $VECTOR_ENDPOINT ]
-then
-    cp -f sink-vector.yaml.template sink-vector.yaml
-    CONFIG_FILES="$CONFIG_FILES sink-vector.yaml"
-    export VECTOR_TLS_ENABLED="false"
-    if [ ! -z $VECTOR_TLS_CA_FILE ]
-    then
-        echo ${VECTOR_TLS_CA_FILE} | base64 -d > ${CERTIFICATES_DIR}/ca.pem
-        sed -i "s|# ca_file: |ca_file: ${CERTIFICATES_DIR}/ca.pem|g" sink-vector.yaml
-        export VECTOR_TLS_ENABLED="true"
-    fi
-    if [ ! -z $VECTOR_TLS_CRT_FILE ] && [ ! -z $VECTOR_TLS_KEY_FILE ]
-    then
-        echo ${VECTOR_TLS_CRT_FILE} | base64 -d > ${CERTIFICATES_DIR}/client.pem
-        echo ${VECTOR_TLS_KEY_FILE} | base64 -d > ${CERTIFICATES_DIR}/client-key.pem
-        sed -i "s|# crt_file: |crt_file: ${CERTIFICATES_DIR}/client.pem|g" sink-vector.yaml
-        sed -i "s|# key_file: |key_file: ${CERTIFICATES_DIR}/client-key.pem|g" sink-vector.yaml
-        export VECTOR_TLS_ENABLED="true"
-    fi
+if [ -n "${VECTOR_ENDPOINT}" ]; then
+	CONFIG_FILES="${CONFIG_FILES} sink-vector.yaml"
+	if [ -n "${VECTOR_TLS_CA_FILE}" ]
+	then
+		echo "${VECTOR_TLS_CA_FILE}" | base64 -d > "${CERTIFICATES_DIR}/ca.pem"
+		sed -i 's|#ca_file:|ca_file:|g' sink-vector.yaml.template
+		VECTOR_TLS_ENABLED=true
+	fi
+	if [ -n "${VECTOR_TLS_CRT_FILE}" ] && [ -n "${VECTOR_TLS_KEY_FILE}" ]
+	then
+		echo "${VECTOR_TLS_CRT_FILE}" | base64 -d > "${CERTIFICATES_DIR}/client.pem"
+		sed -i 's|#crt_file:|crt_file:|g' sink-vector.yaml.template
+		echo "${VECTOR_TLS_KEY_FILE}" | base64 -d > "${CERTIFICATES_DIR}/client-key.pem"
+		sed -i 's|#key_file:|key_file:|g' sink-vector.yaml.template
+		VECTOR_TLS_ENABLED=true
+	fi
+
+	if [[ "${VECTOR_TLS_ENABLED}" == 'true' ]]; then
+		sed -i 's|#verify_certificate:|verify_certificate:|g' sink-vector.yaml.template
+		sed -i 's|#verify_hostname:|verify_hostname:|g' sink-vector.yaml.template
+	fi
 fi
 
-vector --config $CONFIG_FILES
+# https://stackoverflow.com/a/31926346/1559300
+cat < sink-vector.yaml.template | envsubst > sink-vector.yaml
+
+# https://vector.dev/docs/administration/validating/
+cat ${CONFIG_FILES} && (vector validate ${CONFIG_FILES} || sleep "$((RANDOM%10+1))s")
+
+# shellcheck disable=SC2086
+vector --config ${CONFIG_FILES}

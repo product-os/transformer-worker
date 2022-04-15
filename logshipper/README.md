dbd21b5a0dcbde9286254f03837e7df6923e3a46
Carlo Miguel F. Cruz <carloc@balena.io>
2022-04-15 11:54:55 +0000

    logshipper: refactor start script and fixup docs.
    Change-type: patch
    Signed-off-by: Carlo Miguel F. Cruz <carloc@balena.io>

--- logshipper/README.md
+++ logshipper/README.md
@@ -4,15 +4,20 @@ A log collection agent that ships logs from the balena engine
 to a downstream log aggregator.
 
 The agent currently ships logs to the following log aggregators:
-- Vector
+- [Vector][vector]
 
 
 ## Usage
 
-To use this image, create a service in your docker-compose.yml file as shown below:
+To use this image, create a service in your `docker-compose.yml` as shown below:
 ```
+version: "2.1"
+
+volumes:
+  logshipper: {}
+
 logshipper:
-    image: bh.cr/product-os/logshipper
+    image: bh.cr/balena/logshipper
     labels:
       io.balena.features.journal-logs: '1'
     restart: unless_stopped
@@ -20,12 +25,15 @@ logshipper:
       - logshipper:/var/lib/logshipper
 ```
 
-You can also set your docker-compose.yml to build a Dockerfile.template file, and use the build variable %%BALENA_ARCH%% so that the correct image is automatically built for your device arch (see supported architectures):
-
+You can also set your docker-compose.yml to build from a Dockerfile.template file. 
+You may add your own Vector sinks that takes `balena` as input.
 
 *docker-compose.yml*:
 ```
 version: "2.1"
+
+volumes:
+  logshipper: {}
 
 services:
   logshipper:
@@ -37,23 +45,35 @@ services:
       - logshipper:/var/lib/logshipper
 ```
 
-
 *Dockerfile.template*
 ```
-FROM bh.cr/product-os/logshipper:%%BALENA_ARCH%%
+FROM bh.cr/balena/logshipper
+
+COPY sink.yaml /etc/vector
+```
+
+*sink.yaml*
+```
+sinks:
+  console:
+    type: console
+    inputs:
+      - balena
 ```
 
 
 ## Customisation
 
-`balenablocks/logshipper` can be configured via the following variables:
+`bh.cr/balena/logshipper` can be configured via the following variables:
 
 | Environment Variable        | Default| Description                                          |
 | --------------------------- | ------ | -----------------------------------------------------|
 | `VECTOR_ENDPOINT`           | ``     | The endpoint of the vector log aggregator            |
 | `VECTOR_TLS_CA_FILE`        | ``     | An additional CA certificate file encoded in base 64 |
-| `VECTOR_VERIFY_CERTIFICATE` | `true` | Enables TLS certificate verification.                |
+| `VECTOR_VERIFY_CERTIFICATE` | `false` | Enables TLS certificate verification.                |
 
 You can refer to the [docs](https://www.balena.io/docs/learn/manage/serv-vars/#environment-and-service-variables) on how to set environment or service variables
 
 Alternatively, you can set them in the `docker-compose.yml` or `Dockerfile.template` files.
+
+[vector]: https://vector.dev
--- logshipper/start.sh
+++ logshipper/start.sh
@@ -1,40 +1,47 @@
-#!/bin/bash
+#!/usr/bin/env bash
 # shellcheck disable=SC2034,SC2086
 
 set -ae
 
-cd /etc/vector/
+function prepare_vector_sink() {
+	VECTOR_TLS_ENABLED=false
+	if [ -n "${VECTOR_ENDPOINT}" ]; then
+		CONFIG_FILES="${CONFIG_FILES} sink-vector.yaml"
+		if [ -n "${VECTOR_TLS_CA_FILE}" ]
+		then
+			echo "${VECTOR_TLS_CA_FILE}" | base64 -d > "${CERTIFICATES_DIR}/ca.pem"
+			sed -i 's|#ca_file:|ca_file:|g' sink-vector.yaml.template
+			VECTOR_TLS_ENABLED=true
+			if [ -n "${VECTOR_TLS_CRT_FILE}" ] && [ -n "${VECTOR_TLS_KEY_FILE}" ]
+			then
+				echo "${VECTOR_TLS_CRT_FILE}" | base64 -d > "${CERTIFICATES_DIR}/client.pem"
+				sed -i 's|#crt_file:|crt_file:|g' sink-vector.yaml.template
+				echo "${VECTOR_TLS_KEY_FILE}" | base64 -d > "${CERTIFICATES_DIR}/client-key.pem"
+				sed -i 's|#key_file:|key_file:|g' sink-vector.yaml.template
+				VECTOR_TLS_ENABLED=true
+			fi
+		fi
+
+		if [[ "${VECTOR_TLS_ENABLED}" == 'true' ]]; then
+			sed -i 's|#verify_certificate:|verify_certificate:|g' sink-vector.yaml.template
+			sed -i 's|#verify_hostname:|verify_hostname:|g' sink-vector.yaml.template
+		fi
+		# https://stackoverflow.com/a/31926346/1559300
+		cat < sink-vector.yaml.template | envsubst > sink-vector.yaml
+fi
+}
+
+function start_vector() {
+	# https://vector.dev/docs/administration/validating/
+	cat ${CONFIG_FILES} \
+	&& vector validate --config-dir /etc/vector \
+	&& vector --config-dir /etc/vector 
+}
+
 
 BALENA_FLEET_NAME=${BALENA_APP_NAME}
 CERTIFICATES_DIR=/etc/vector/certificates
-VECTOR_TLS_ENABLED=false
 
-if [ -n "${VECTOR_ENDPOINT}" ]; then
-	if [ -n "${VECTOR_TLS_CA_FILE}" ]
-	then
-		echo "${VECTOR_TLS_CA_FILE}" | base64 -d > "${CERTIFICATES_DIR}/ca.pem"
-		sed -i 's|#ca_file:|ca_file:|g' sink-vector.yaml.template
-		VECTOR_TLS_ENABLED=true
-	fi
-	if [ -n "${VECTOR_TLS_CRT_FILE}" ] && [ -n "${VECTOR_TLS_KEY_FILE}" ]
-	then
-		echo "${VECTOR_TLS_CRT_FILE}" | base64 -d > "${CERTIFICATES_DIR}/client.pem"
-		sed -i 's|#crt_file:|crt_file:|g' sink-vector.yaml.template
-		echo "${VECTOR_TLS_KEY_FILE}" | base64 -d > "${CERTIFICATES_DIR}/client-key.pem"
-		sed -i 's|#key_file:|key_file:|g' sink-vector.yaml.template
-		VECTOR_TLS_ENABLED=true
-	fi
-
-	if [[ "${VECTOR_TLS_ENABLED}" == 'true' ]]; then
-		sed -i 's|#verify_certificate:|verify_certificate:|g' sink-vector.yaml.template
-		sed -i 's|#verify_hostname:|verify_hostname:|g' sink-vector.yaml.template
-	fi
-fi
-
-# https://stackoverflow.com/a/31926346/1559300
-cat < sink-vector.yaml.template | envsubst > sink-vector.yaml
-
-# https://vector.dev/docs/administration/validating/
-cat /etc/vector/*.yaml \
-&& vector validate --config-dir /etc/vector \
-&& vector --config-dir /etc/vector
+cd /etc/vector
+prepare_vector_sink
+start_vector
